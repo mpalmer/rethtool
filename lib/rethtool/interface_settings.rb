dce5d747c1ea654ff6c4430a2fa4c6337f7e9527
Aviram Bar-Haim <aviramb@mellanox.com>
2014-11-20 15:17:25 +0200

    Added interface driver and bus_info support
    Signed-off-by: Aviram Bar-Haim <aviramb@mellanox.com>

--- lib/rethtool/interface_settings.rb
+++ lib/rethtool/interface_settings.rb
@@ -37,10 +37,25 @@ class Rethtool::InterfaceSettings
 	# interface you want to get the settings for.
 	def initialize(interface)
 		cmd = Rethtool::EthtoolCmd.new
+		cmd_driver = Rethtool::EthtoolCmdDriver.new
 		cmd.cmd = Rethtool::ETHTOOL_CMD_GSET
+		cmd_driver.cmd = Rethtool::ETHTOOL_CMD_GDRVINFO
 		
 		@data = Rethtool.ioctl(interface, cmd)
+		@driver_data = Rethtool.ioctl(interface, cmd_driver)
 	end
+
+        def as_str(str)
+		str.pack('c*').delete("\000")
+        end
+
+        def driver
+		self.as_str(@driver_data.driver)
+        end
+
+        def bus_info
+		self.as_str(@driver_data.bus_info)
+        end
 	
 	# Return an array of the modes supported by the interface.  Returns an
 	# array of Mode objects.
